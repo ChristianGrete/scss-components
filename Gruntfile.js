c262552451f85b53c72ce436154a7406a6d83a5c
Tim Hartmann <tim.hartmann93@gmail.com>
2014-02-24 18:25:00 +0100

    refactor(tests): removing tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      options: {
-        livereload: true
-      },
-      files: [
-        'tests/**'
-      ],
-      tasks: ['default']
-    },
-    sass: {
-      test: {
-        options: {
-          style: 'expanded',
-          compass: true
-        },
-        files: {
-          'tests/animations/assets/stylesheets/application.css' : 'tests/animations/assets/scss/application.scss',
-          'tests/completions/assets/stylesheets/application.css' : 'tests/completions/assets/scss/application.scss',
-          'tests/grid/assets/stylesheets/application.css' : 'tests/grid/assets/scss/application.scss',
-          'tests/icons/assets/stylesheets/application.css' : 'tests/icons/assets/scss/application.scss'
-        }
-      }
-    },
     bumpup: ['package.json', 'bower.json'],
     tagrelease: {
       file: 'package.json',
